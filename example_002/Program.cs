0e78ca5ef98adac3327d1db383705e5dadca6e0b
Anastasia <super.okay335@yandex.ru>
2022-09-11 23:08:00 +0300

    добавили решение 2 задачи

--- example_002/Program.cs
+++ example_002/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+void TochkaPoint(double k1, double b1,double k2, double b2)
+{
+double x = (b2-b1)/(k1-k2);
+double y = k1*x + b1;
+if(k1==k2) Console.Write("Заданные прямые не пересекаются!");
+else
+Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
+}
+
+TochkaPoint(k1,b1,k2,b2);
+
+
