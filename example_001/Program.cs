12a74e99f3a9df46abf68306586424784ab35ad6
Anastasia <super.okay335@yandex.ru>
2022-09-11 22:24:47 +0300

    добавили решение 1 задачи

--- example_001/Program.cs
+++ example_001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите колличество чисел: ");
+int n = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
